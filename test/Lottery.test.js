b9c1b3943199f159b165138e896be10529fc0679
ashrafstakala <ashrafstakala@gmail.com>
2021-12-22 21:21:25 +0200

    added end to end test case

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -83,4 +83,18 @@ describe('Lottery Contract', () => {
       assert(err);
     }
   });
+
+  // End to end test, using only one player to keep test case code concise
+  it('sends money to the winner and resets the players array', async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('2', 'ether'),
+    });
+
+    const initialBalance = await web3.eth.getBalance(accounts[0]);
+    await lottery.methods.pickWinner().send({ from: accounts[0] });
+    const finalBalance = await web3.eth.getBalance(accounts[0]);
+    const difference = finalBalance - initialBalance;
+    assert(difference > web3.utils.toWei('1.9', 'ether')); // factoring in the gas cost
+  });
 });
