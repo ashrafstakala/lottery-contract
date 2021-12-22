ec475a4820a35afe6a4573b81f70d164c8d34f17
ashrafstakala <ashrafstakala@gmail.com>
2021-12-22 20:03:24 +0200

    added first test case

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -1,10 +1,9 @@
-const { isValidProvider } = require("@truffle/hdwallet-provider");
-const assert = require("assert");
-const ganache = require("ganache-cli");
-const Web3 = require("web3");
-const web3 = new Web3(ganache.Provider());
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
 
-const { interface, bytecode } = require("../compile");
+const { interface, bytecode } = require('../compile');
 
 let lottery;
 let accounts;
@@ -13,6 +12,12 @@ beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
 
   lottery = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ date: bytecode })
-    .send({ from: accounts[0], gas: "1000000" });
+    .deploy({ data: bytecode })
+    .send({ from: accounts[0], gas: '1000000' });
+});
+
+describe('Lottery Contract', () => {
+  it('deploys a contract', () => {
+    assert.ok(lottery.options.address);
+  });
 });
